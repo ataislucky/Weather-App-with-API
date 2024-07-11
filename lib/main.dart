ac5fd0b7a360fb6c7df4e0b6472f03a5f902239a
ataislucky <ataislucky@gmail.com>
2024-07-11 18:13:21 +0700

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:weather_app/services/weather_services.dart';
+import 'package:weather_app/widget/weather_data_tile.dart';
 
 void main() {
   runApp(const WeatherApp());
@@ -28,49 +32,131 @@ class WeatherPage extends StatefulWidget {
 }
 
 class WeatherPageState extends State<WeatherPage> {
+  final TextEditingController _controller = TextEditingController();
+  String _bgImg = 'assets/images/clear.jpg';
+  String _iconImg = 'assets/icons/Clear.png';
+  String _cityName = '';
+  String _temperature = '';
+  String _tempMax = '';
+  String _tempMin = '';
+  String _sunrise = '';
+  String _sunset = '';
+  String _main = '';
+  String _presure = '';
+  String _humidity = '';
+  String _visibility = '';
+  String _windSpeed = '';
+
+  getData(String cityName) async {
+    final weatherService = WeatherService();
+    var weatherData;
+    if (cityName == '') {
+      weatherData = await weatherService.fetchWeather();
+    } else {
+      weatherData = await weatherService.getWeather(cityName);
+    }
+
+    debugPrint(weatherData.toString());
+    setState(() {
+      _cityName = weatherData['name'];
+      _temperature = weatherData['main']['temp'].toStringAsFixed(1);
+      _main = weatherData['weather'][0]['main'];
+      _tempMax = weatherData['main']['temp_max'].toStringAsFixed(1);
+      _tempMin = weatherData['main']['temp_min'].toStringAsFixed(1);
+      _sunrise = DateFormat('hh:mm a').format(
+          DateTime.fromMillisecondsSinceEpoch(
+              weatherData['sys']['sunrise'] * 1000));
+      _sunset = DateFormat('hh:mm a').format(
+          DateTime.fromMillisecondsSinceEpoch(
+              weatherData['sys']['sunset'] * 1000));
+      _presure = weatherData['main']['pressure'].toString();
+      _humidity = weatherData['main']['humidity'].toString();
+      _visibility = weatherData['visibility'].toString();
+      _windSpeed = weatherData['wind']['speed'].toString();
+
+      if (_main == 'Clear') {
+        _bgImg = 'assets/images/clear.jpg';
+        _iconImg = 'assets/icons/Clear.png';
+      } else if (_main == 'Clouds') {
+        _bgImg = 'assets/images/clouds.jpg';
+        _iconImg = 'assets/icons/Clouds.png';
+      } else if (_main == 'Rain') {
+        _bgImg = 'assets/images/rain.jpg';
+        _iconImg = 'assets/icons/Rain.png';
+      } else if (_main == 'Fog') {
+        _bgImg = 'assets/images/fog.jpg';
+        _iconImg = 'assets/icons/Haze.png';
+      } else if (_main == 'Thunderstorm') {
+        _bgImg = 'assets/images/thunderstorm.jpg';
+        _iconImg = 'assets/icons/Thunderstorm.png';
+      } else {
+        _bgImg = 'assets/images/haze.jpg';
+        _iconImg = 'assets/icons/Haze.png';
+      }
+    });
+  }
+
+  Future<bool> _checkLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return false;
+      }
+      getData('');
+    }
+    getData('');
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Stack(
       children: [
         Image.asset(
-          'assets/images/haze.jpg',
+          'assets/images/fog.jpg',
           fit: BoxFit.cover,
           width: double.infinity,
           height: double.infinity,
         ),
-        const Padding(
-          padding: EdgeInsets.all(15),
+        Padding(
+          padding: const EdgeInsets.all(15),
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 40,
                 ),
                 TextField(
-                  decoration: InputDecoration(
+                  controller: _controller,
+                  onChanged: (value) {
+                    getData(value);
+                  },
+                  decoration: const InputDecoration(
                       suffixIcon: Icon(Icons.search),
                       filled: true,
                       fillColor: Colors.black26,
-                      hintText: 'Jeneng Kuthoe opo Cak?',
+                      hintText: 'Enter city name',
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(16)))),
                 ),
-                SizedBox(height: 15),
+                const SizedBox(height: 15),
                 Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Icon(Icons.location_on),
+                  const Icon(Icons.location_on),
                   Text(
-                    'Gresik',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    _cityName,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.w500),
                   )
                 ]),
-                SizedBox(
+                const SizedBox(
                   height: 50,
                 ),
                 Text(
-                  '30.9°C',
-                  style: TextStyle(
+                  '$_temperature°C',
+                  style: const TextStyle(
                       color: Colors.black,
                       fontSize: 90,
                       fontWeight: FontWeight.bold),
@@ -78,53 +164,88 @@ class WeatherPageState extends State<WeatherPage> {
                 Row(
                   children: [
                     Text(
-                      'Haze',
-                      style:
-                          TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
+                      _main,
+                      style: const TextStyle(
+                          fontSize: 40, fontWeight: FontWeight.w500),
                     ),
                     //if using 'Image.asset('assets/icons/Haze.png')' will get error
-                    Image(
-                      image: AssetImage('assets/icons/Haze.png'),
-                      height: 60,
+                    Image.asset(
+                      _iconImg,
+                      height: 80,
                     )
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 25,
                 ),
                 Row(
                   children: [
-                    Icon(Icons.arrow_upward),
+                    const Icon(Icons.arrow_upward),
                     Text(
-                      '35°C',
-                      style:
-                          TextStyle(fontSize: 22, fontStyle: FontStyle.italic),
+                      '$_tempMax°C',
+                      style: const TextStyle(
+                          fontSize: 22, fontStyle: FontStyle.italic),
                     ),
-                    Icon(Icons.arrow_downward),
+                    const Icon(Icons.arrow_downward),
                     Text(
-                      '25°C',
-                      style:
-                          TextStyle(fontSize: 22, fontStyle: FontStyle.italic),
+                      '$_tempMin°C',
+                      style: const TextStyle(
+                          fontSize: 22, fontStyle: FontStyle.italic),
                     )
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 25,
                 ),
                 Card(
-                  elevation: 5,
+                  elevation: 2,
                   //please deh if using 'child: Container' will get error
-                  child: ColoredBox(
-                    color: Colors.transparent,
+                  //child: ColoredBox(
+                  color: Colors.transparent,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
                     child: Column(
                       children: [
-                        Row(
-                          children: [Text('Sunrise'), Text('Sunsite')],
-                        )
+                        WeatherDataTile(
+                            index1: "Sunrise",
+                            index2: "Sunset",
+                            value1: _sunrise,
+                            value2: _sunset),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        WeatherDataTile(
+                            index1: "Humidity",
+                            index2: "Visibility",
+                            value1: _humidity,
+                            value2: _visibility),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        WeatherDataTile(
+                            index1: "Pressure",
+                            index2: "Wind speed",
+                            value1: _presure,
+                            value2: _windSpeed),
+                        const SizedBox(
+                          height: 2,
+                        ),
                       ],
                     ),
                   ),
-                )
+                ),
+                const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Developed by Ata Amrullah',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ]),
               ],
             ),
           ),
